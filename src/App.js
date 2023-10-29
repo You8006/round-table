bcb5bb1114743d27d37fc43b1d199ff5751d632c
Youhei <kagkaggru@gmail.com>
2023-10-29 11:40:27 +0900

    c

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import Title from "./component/Title";
+import Bar from "./component/bar";
+import Boton from "./component/boton";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Title></Title>
+
+      <Boton></Boton>
+      <Bar></Bar>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/component/Boton.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./boton.css";
+
+import React from "react";
+
+const Boton = () => {
+  return <div>Boton</div>;
+};
+
+export default Boton;
